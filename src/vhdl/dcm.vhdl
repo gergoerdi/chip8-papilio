c2f2a39b837711cc40b17c64e2c5ab868d1f3d2d
Dr. ERDI Gergo <gergo@erdi.hu>
2014-02-08 11:05:51 +0800

    Simplify by using direct instantiation

--- src/vhdl/dcm.vhdl
+++ src/vhdl/dcm.vhdl
@@ -10,16 +10,8 @@ entity dcm is
 end dcm;
 
 architecture arch of dcm is
-  component dcm_32_to_50p35
-    port(
-      clkin_in : in std_logic;          
-      clkfx_out : out std_logic;
-      clkin_ibufg_out : out std_logic;
-      clk0_out : out std_logic
-      );
-  end component;
 begin
-  inst_dcm_32_to_50p35: dcm_32_to_50p35
+  inst_dcm_32_to_50p35: entity work.dcm_32_to_50p35
     port map(
       clkin_in => CLK_32MHZ,
       clkfx_out => CLK_50MHZ,
--- src/vhdl/main.vhdl
+++ src/vhdl/main.vhdl
@@ -10,7 +10,7 @@ entity main is
     BTN_LEFT : in std_logic;
     BTN_RIGHT : in std_logic;
     BTN_RESET : in std_logic;
-    --LED : out std_logic_vector(3 downto 0);
+    -- LED : out std_logic_vector(3 downto 0);
     VGA_VSYNC : out std_logic;
     VGA_HSYNC : out std_logic;
     VGA_R : out std_logic_vector(3 downto 0);
@@ -22,37 +22,14 @@ end main;
 architecture arch of main is
   signal CLK_50MHZ : std_logic;
   
-  component dcm
-    port(
-      CLK_32MHZ : in std_logic;          
-      CLK_50MHZ : out std_logic
-      );
-  end component;
-
-  component Video
-    port(CLK_50MHZ : in std_logic;
-         BTN_UP : in std_logic;
-         BTN_DOWN : in std_logic;
-         BTN_LEFT : in std_logic;
-         BTN_RIGHT : in std_logic;
-         BTN_RESET : in std_logic;
-         --LED : out std_logic_vector(3 downto 0);
-         VGA_VSYNC : out std_logic;
-         VGA_HSYNC : out std_logic;
-         VGA_R : out std_logic_vector(3 downto 0);
-         VGA_G : out std_logic_vector(3 downto 0);
-         VGA_B : out std_logic_vector(3 downto 0)
-         );
-    end component;
-  
 begin
-  inst_dcm: dcm
+  inst_dcm: entity work.dcm
     port map(
       CLK_32MHZ => CLK_32MHZ,
       CLK_50MHZ => CLK_50MHZ
       );
 
-  inst_Video: Video
+  inst_Video: entity work.Video
     port map(
       CLK_50MHZ => CLK_50MHZ,
       BTN_UP => BTN_UP,
@@ -60,7 +37,7 @@ begin
       BTN_LEFT => BTN_LEFT,
       BTN_RIGHT => BTN_RIGHT,
       BTN_RESET => BTN_RESET,
-      --LED => LED,
+      -- LED => LED,
       VGA_VSYNC => VGA_VSYNC,
       VGA_HSYNC => VGA_HSYNC,
       VGA_R => VGA_R,
